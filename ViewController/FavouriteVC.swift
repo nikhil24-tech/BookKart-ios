1346989b01dd8b4b6d259ca236bba4240d18a838
sukhwindersingh7 <jasss73904@gmail.com>
2022-05-18 10:13:56 -0400

    Favourite and Profile Updated.

--- ViewController/FavouriteVC.swift
+++ ViewController/FavouriteVC.swift
@@ -9,31 +9,123 @@ class FavouriteVC: UIViewController {
 
     
     @IBOutlet weak var tblFavList: UITableView!
-    @IBOutlet weak var txtSearch: UITextField!
+    @IBOutlet weak var sbBar: UISearchBar!
 
+    
+    var pendingItem: DispatchWorkItem?
+    var pendingRequest: DispatchWorkItem?
+    var array = [BookModel]()
+    var arrData = [BookModel]()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.tblFavList.delegate = self
-        self.tblFavList.dataSource = self
-        self.txtSearch.layer.borderWidth = 1.0
-        self.txtSearch.layer.borderColor = UIColor.lightGray.cgColor
-        self.txtSearch.layer.cornerRadius = 10.0
+        self.sbBar.layer.borderWidth = 1.0
+        self.sbBar.layer.borderColor = UIColor.lightGray.cgColor
+        self.sbBar.layer.cornerRadius = 10.0
+        self.sbBar.delegate = self
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let userEmail = GFunction.user.email {
+            self.getData(email: userEmail)
+        }
     }
 }
 
 
 extension FavouriteVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        5
+        self.arrData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath) as! BookCell
-        
+        let data = self.arrData[indexPath.row]
+        cell.config(data: data)
+        cell.btnAdd.addAction(for: .touchUpInside) {
+            if let userEmail = GFunction.user.email {
+                self.checkCart(data: data, email: userEmail)
+            }
+        }
+        cell.btnFav.addAction(for: .touchUpInside) {
+//            if let userEmail = GFunction.user.email {
+//                self.checkFav(data: data, email: userEmail)
+//            }
+            
+            self.deleteFav(docID: data.docId)
+        }
         cell.selectionStyle = .none
         return cell
+    }
+    
+    func cart(data: BookModel,email:String) {
+        var ref : DocumentReference? = nil
+        ref = AppDelegate.shared.db.collection(bCart).addDocument(data:
+            [
+                bName: data.name,
+                bCategory : data.category,
+                bAddedDate: data.addedDate,
+                bPrice: data.price,
+                bEmail: email
+            ])
+        {  err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+                Alert.shared.showAlert(message: "Your Book has been Added into cart !!!") { (true) in
+                    UIApplication.shared.setTab()
+                }
+            }
+        }
+    }
+    
+    func deleteFav(docID:String){
+        let ref = AppDelegate.shared.db.collection(bFavourite).document(docID)
+        ref.delete(){ err in
+            if let err = err {
+                print("Error updating document: \(err)")
+                self.navigationController?.popViewController(animated: true)
+            } else {
+                print("Document successfully updated")
+                Alert.shared.showAlert(message: "Your Book has been removed from favourite !!!") { (true) in
+                    UIApplication.shared.setTab()
+                }
+            }
+        }
+    }
+    
+    func checkFav(data: BookModel,email:String){
+        _ = AppDelegate.shared.db.collection(bFavourite).whereField(bEmail, isEqualTo: email).addSnapshotListener{ querySnapshot, error in
+            
+            guard let snapshot = querySnapshot else {
+                print("Error fetching snapshots: \(error!)")
+                return
+            }
+            if snapshot.documents.count != 0 {
+                let docID = snapshot.documents[0].documentID
+                self.deleteFav(docID: docID)
+            }
+        }
+    }
+    
+    func checkCart(data: BookModel,email:String){
+        _ = AppDelegate.shared.db.collection(bCart).whereField(bBookID, isEqualTo: data.docId).addSnapshotListener{ querySnapshot, error in
+            
+            guard let snapshot = querySnapshot else {
+                print("Error fetching snapshots: \(error!)")
+                return
+            }
+            if snapshot.documents.count == 0 {
+                self.cart(data: data, email: email)
+            }else{
+                Alert.shared.showAlert(message: "Book already in cart list !!!", completion: nil)
+            }
+        }
     }
 }
 
@@ -57,5 +149,83 @@ class BookCell: UITableViewCell {
         self.vwCell.layer.borderColor = UIColor.lightGray.cgColor
         self.vwCell.backgroundColor = .clear
         self.btnAdd.layer.cornerRadius = 10.0
+    }
+    
+    func config(data: BookModel){
+        self.lblPrice.text = "$ \(data.price.description)"
+        self.lblName.text = data.name.description
+        self.lblDate.text = data.addedDate.description
+    }
+}
+
+
+extension FavouriteVC {
+    func getData(email:String){
+        _ = AppDelegate.shared.db.collection(bFavourite).whereField(bEmail, isEqualTo: email).addSnapshotListener{ querySnapshot, error in
+            
+            guard let snapshot = querySnapshot else {
+                print("Error fetching snapshots: \(error!)")
+                return
+            }
+            self.array.removeAll()
+            if snapshot.documents.count != 0 {
+                for data in snapshot.documents {
+                    let data1 = data.data()
+                    if let name: String = data1[bName] as? String, let addedDate: String = data1[bAddedDate] as? String, let price: String = data1[bPrice] as? String, let category: String = data1[bCategory] as? String {
+                        print("Data Count : \(self.array.count)")
+                        self.array.append(BookModel(docId: data.documentID, name: name, category: category, price: price, addedDate: addedDate))
+                    }
+                }
+            }
+            self.arrData = self.array
+            self.tblFavList.delegate = self
+            self.tblFavList.dataSource = self
+            self.tblFavList.reloadData()
+        }
+    }
+}
+
+
+//MARK:- UISearchBarDelegate Delegate methods :-
+extension FavouriteVC : UISearchBarDelegate{
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        
+        self.pendingRequest?.cancel()
+        
+        guard searchBar.text != nil else {
+            return
+        }
+        
+        if(searchText.count == 0 || (searchText == " ")){
+            self.arrData = self.array
+            self.tblFavList.reloadData()
+            return
+        }
+        
+        //self.isTextEdit = true
+        
+        self.pendingRequest = DispatchWorkItem{ [weak self] in
+            
+            guard let self = self else { return }
+            
+            self.arrData = self.array.filter({$0.name.localizedStandardContains(searchText)})
+            self.tblFavList.reloadData()
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(300), execute: self.pendingRequest!)
+        
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.arrData = self.array.filter({$0.name.localizedStandardContains(searchBar.text!)})
+        self.tblFavList.reloadData()
+        self.sbBar.resignFirstResponder()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        self.arrData = self.array
+        self.sbBar.resignFirstResponder()
     }
 }
--- ViewController/ProfileVC.swift
+++ ViewController/ProfileVC.swift
@@ -1,15 +1,14 @@
 //
 //  ProfileVC.swift
-//  BookKart
-//
-//  Created by 2022M3 on 17/04/22.
-//
 
 import UIKit
 
 class ProfileVC: UIViewController {
 
     
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblEmail: UILabel!
+    @IBOutlet weak var lblAddress: UILabel!
     @IBOutlet weak var btnSignOut: UIButton!
     
     
@@ -21,7 +20,11 @@ class ProfileVC: UIViewController {
         super.viewDidLoad()
 
         self.btnSignOut.layer.cornerRadius = 10.0
-        
+        if GFunction.user != nil {
+            self.lblName.text = GFunction.user.name
+            self.lblAddress.text = GFunction.user.address
+            self.lblEmail.text = GFunction.user.email
+        }
         // Do any additional setup after loading the view.
     }
 
